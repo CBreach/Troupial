6f741bbaa9a9ed9af5807f5069eacd1ef1d8bed1
CBreach <cbreach@sandiego.edu>
2026-01-11 16:30:10 -0800

    added headers for the collector and began the logic of for parsing the HTML

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-	//"github.com/CBreach/Troupial/fetcher"
+	"log"
+
+	"github.com/CBreach/Troupial/fetcher"
 	//"github.com/gocolly/colly/v2"
 )
 
 func main() {
 	fmt.Println("PIpeline begins")
-
+	f, err := fetcher.NewFetcher("https://www.linkedin.com/jobs/collections/recommended/?currentJobId=3864453531")
+	if err != nil {
+		log.Fatalf("Could not initialize fetcher, error: %v", err)
+	}
+	f.Fetch()
 }
--- fetcher/job_site.go
+++ fetcher/job_site.go
@@ -30,12 +30,32 @@ func NewFetcher(rawUrl string) (*Fetcher, error) {
 	if err != nil {
 		return nil, err
 	}
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		//adding headers to avoid beign blocked
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.5")
+		fmt.Printf("Visiting: %v\n", r.URL)
+	})
+
+	//need to make the logic based on the host that is being visited
 
 	// this is a bit of a weird syntax but it basically initializes a new fetcher and returns its address along with a null error
 	return &Fetcher{
 		rawUrl:    rawUrl,
 		Host:      host,
-		Collector: colly.NewCollector(),
+		Collector: c,
 	}, nil
 
+}
+
+// this function is where the actual web fetching happens
+// the fetcher object calling the structur already has
+func (f *Fetcher) Fetch() {
+	f.Collector.OnRequest(func(r *colly.Request) {
+		fmt.Printf("visiting %s", f.Host)
+	})
 }
